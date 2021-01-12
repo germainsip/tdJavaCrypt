7b4cf2abc15dc707d00aeb6d87d47c29858242bc
Germain SIPIERE <43010217+germainsip@users.noreply.github.com>
2021-01-12 09:05:16 +0100

    main ok

--- src/com/afpa/Main.java
+++ src/com/afpa/Main.java
@@ -1,8 +1,46 @@
 package com.afpa;
 
+import com.afpa.GUI.Menu;
+import com.afpa.model.Message;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        Menu menu = new Menu();
+        String test = menu.init();
+        String home = System.getProperty("user.dir");
+
+
+
+
+        if (!test.equals("quit")){
+            Path msgClearPath;
+            Path msgEncodedPath;
+            Path msgKeyPath;
+            Message message;
+            Map<String,String> choix = menu.pathChooser(test);
+            //Path msgClearPath = Paths.get(home,"decoded.txt");
+            switch (test) {
+                case "decode" -> {
+                    msgClearPath = Paths.get(home, "decoded.txt");
+                    msgEncodedPath = Paths.get(home, choix.get("encodedFile") + ".txt");
+                    msgKeyPath = Paths.get(home, choix.get("keyFile") + ".txt");
+                    message = new Message(true, msgClearPath, msgEncodedPath, msgKeyPath);
+                    message.readNwrite();
+                }
+                case "encode" -> {
+                    msgClearPath = Paths.get(home, choix.get("clearFile") + ".txt");
+                    msgEncodedPath = Paths.get(home, "encoded.txt");
+                    msgKeyPath = Paths.get(home, choix.get("keyFile") + ".txt");
+                    message = new Message(false, msgClearPath, msgEncodedPath, msgKeyPath);
+                    message.readNwrite();
+                }
+            }
+        }
     }
 }
