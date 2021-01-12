3ed6da5877417ce134bc91ec6b418d7c81ea4e47
Germain SIPIERE <43010217+germainsip@users.noreply.github.com>
2021-01-12 09:02:27 +0100

    ajout Menu

--- src/com/afpa/GUI/Menu.java
+++ src/com/afpa/GUI/Menu.java
@@ -7,31 +7,28 @@ import java.util.Scanner;
 public class Menu {
     /**
      * Initialisation du menu
-     * @return
+     * @return choix utilisateur
      */
     public String init(){
         Scanner sc = new Scanner(System.in);
         String retour;
         String sortie = null;
         do {
-            System.out.println("╔══════════════════════════════════════╗\n" +
-                    "║   System d'encodage et de décodage   ║ \n" +
-                    "║             de messages              ║ \n" +
-                    "╠══════════════════════════════════════╣ \n" +
-                    "║       1) Décoder un message          ║ \n" +
-                    "║       2) Encode un message           ║ \n" +
-                    "║       3) Quitter                     ║ \n" +
-                    "╚══════════════════════════════════════╝");
+            System.out.println("""
+                    ╔══════════════════════════════════════╗
+                    ║   System d'encodage et de décodage   ║\s
+                    ║             de messages              ║\s
+                    ╠══════════════════════════════════════╣\s
+                    ║       1) Décoder un message          ║\s
+                    ║       2) Encode un message           ║\s
+                    ║       3) Quitter                     ║\s
+                    ╚══════════════════════════════════════╝""");
 
             retour = sc.next();
-            switch (retour){
-                case "1": sortie = "decode";
-                    break;
-                case "2": sortie = "encode";
-                    break;
-                case "3": sortie = "quit";
-                    break;
-
+            switch (retour) {
+                case "1" -> sortie = "decode";
+                case "2" -> sortie = "encode";
+                case "3" -> sortie = "quit";
             }
 
         } while(sortie == null);
@@ -42,19 +39,19 @@ public class Menu {
     public Map<String, String> pathChooser(String type){
         Scanner sc = new Scanner(System.in);
         Map<String, String> pathChoices = new HashMap<>();
-        switch (type){
-            case "decode":
+        switch (type) {
+            case "decode" -> {
                 System.out.println("Entrez le nom du fichier à décoder (sans extension)");
-                pathChoices.put("encodedFile",sc.next());
+                pathChoices.put("encodedFile", sc.next());
                 System.out.println("Entrez le nom du fichier contenant la clé de décodage (sans extension)");
                 pathChoices.put("keyFile", sc.next());
-                break;
-            case "encode":
+            }
+            case "encode" -> {
                 System.out.println("Entrez le nom du fichier à encoder (sans extension)");
-                pathChoices.put("clearFile",sc.next());
+                pathChoices.put("clearFile", sc.next());
                 System.out.println("Entrez le nom du fichier contenant la clé de décodage (sans extension)");
                 pathChoices.put("keyFile", sc.next());
-                break;
+            }
         }
 
         return pathChoices;
