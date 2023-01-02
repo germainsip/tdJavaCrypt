95591350b57c59ea8e64ecb7966d99ee285b35b4
Germain SIPIERE <gsipiere@gmail.com>
2023-01-02 13:47:55 +0100

    remove keypath from attributs

--- src/com/afpa/model/Message.java
+++ src/com/afpa/model/Message.java
@@ -9,12 +9,11 @@ import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class Message {
-    private Boolean encoded;
+    private final Boolean encoded;
     private List<String> msgClear;
     private List<String> msgEncodded;
     private Path msgClearPath;
     private Path msgEncodedPath;
-    private Path keyPath;
     private String key;
     private Transcoder transCoder;
 
@@ -26,13 +25,11 @@ public class Message {
      * @param msgClearPath chemin vers le fichier décodé
      * @param msgEncodedPath chemin vers le fichier codé
      * @param keyPath chemin vers la clé
-     * @throws IOException
      */
     public Message(Boolean encoded, Path msgClearPath, Path msgEncodedPath, Path keyPath) throws IOException {
         this.encoded = encoded;
         this.msgClearPath = msgClearPath;
         this.msgEncodedPath = msgEncodedPath;
-        this.keyPath = keyPath;
         this.key = Files.readString(keyPath);
 
         this.transCoder = new Transcoder(key);
